c717afdefba79b6be6fc76263c169403c7b7e90d
Henrique Moraes <ahmdesouza@gmail.com>
2022-07-24 00:40:31 -0300

    Update script.js

--- aula14/script.js
+++ aula14/script.js
@@ -11,12 +11,43 @@ function verificar() {
         var idade = ano - Number(fano.value)
         
         var gênero = ''
+        var img = document.createElement("img")
+        img.setAttribute("id", "foto")
+
+
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >= 0 && idade <= 10) {
+                //crianca
+                img.setAttribute('src', "./imagens/bebeh.jpg")
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute("src", "./imagens/jovemh.jpg")
+            } else if (idade < 50 ) {
+                //adulto
+                img.setAttribute("src", "./imagens/homem.jpg")
+            } else {
+                //idoso
+                img.setAttribute("src", "./imagens/idoso.jpg")
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >= 0 && idade <= 10) {
+                //crianca
+                img.setAttribute("src", "./imagens/bebem.jpg")
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute("src", "./imagens/jovemm.jpg")
+            } else if (idade < 50 ) {
+                //adulto
+                img.setAttribute("src", "./imagens/mulher.jpg")
+            } else {
+                //idoso
+                img.setAttribute("src", "./imagens/idosa.jpg")
+            }
         }
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
 
 
     }
