fadf74b78423ac4bb00b5931c6dfe7c99f7bddac
Henrique Moraes <ahmdesouza@gmail.com>
2022-07-24 21:34:08 -0300

    repetição
    while, do..while, for

---
+++ aula15/for.js
@@ -0,0 +1,3 @@
+for (var c = 0; c <= 100; c+= 2){
+    console.log(`Printando ${c}`)
+};
---
+++ aula15/whileanddowhile.js
@@ -0,0 +1,14 @@
+var c = 1
+//faz o teste logico antes
+while (c <= 10 ) {
+    console.log(`Printando ${c}`)
+    c++
+};
+
+
+//faz o teste logico depois
+do {
+    console.log(`Printando ${c}`)
+    c++
+
+} while (c <= 10);
