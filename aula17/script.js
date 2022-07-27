fc6afddc11501a1a4e8355a48c58d15910ac142a
Henrique Moraes <ahmdesouza@gmail.com>
2022-07-26 22:09:09 -0300

    create script

---
+++ aula17/script.js
@@ -0,0 +1,12 @@
+function tabuada() {
+    var num = document.getElementById("txtn")
+    var tab = document.getElementById("seltab")
+
+    if (num.value.length == 0) {
+         var n = Number(num.value)
+    } else {
+        window.alert("[ ERRO ] - Digite um número!")
+    }
+   
+
+}
