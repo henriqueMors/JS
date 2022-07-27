96dc51276aff750c128b04600b7b55c4d4704a0d
Henrique Moraes <ahmdesouza@gmail.com>
2022-07-27 18:39:34 -0300

    Create ambiente.js

---
+++ aula18/ambiente.js
@@ -0,0 +1,9 @@
+let num = [ 1 , 9 , 8 ]
+num[0] = 6 // adiciona elemento em um devido lugar
+num.push(7) //adiciona elemento
+num.length
+
+console.log(`Nosso vetor é ${num}`)
+console.log(num.length)
+console.log(num.sort())
+console.log(`Nosso vetor é ${num[2]}`)
