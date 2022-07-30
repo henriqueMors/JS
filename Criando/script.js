ffe59fcbb79ac2743dcba7fb7078c0a239c35847
Henrique Moraes <ahmdesouza@gmail.com>
2022-07-30 12:29:58 -0300

    create criando
    criando um create e testando codigos aleatorios

---
+++ Criando/script.js
@@ -0,0 +1,20 @@
+let alt = document.getElementById("background")
+alt.addEventListener ('mousedown', segurou) 
+
+function gerar_cor(opacidade = 1) {
+    let r = Math.random() * 255;
+    let g = Math.random() * 255;
+    let b = Math.random() * 255;
+ 
+    return `rgba(${r}, ${g}, ${b}, ${opacidade})`;
+ }
+function clicou() {
+    let alt = document.getElementById("background")
+    alt.style.background = gerar_cor()
+    
+}
+
+function segurou() {
+
+    alt.style.background = gerar_cor()
+}
