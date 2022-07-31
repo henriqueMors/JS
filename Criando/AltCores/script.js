0ab746f54c4cfe9625ede80443fa81d2f8befec5
Henrique Moraes <ahmdesouza@gmail.com>
2022-07-31 14:09:53 -0300

    bugtton
    resolvido

--- Criando/AltCores/script.js
+++ Criando/AltCores/script.js
@@ -1,6 +1,3 @@
-let alt = window.document.getElementById("colorir")
-color.addEventListener ('mousedown', segurou)
-
 function gerar_cor(opacidade = 1) {
     let r = Math.random() * 255;
     let g = Math.random() * 255;
@@ -8,13 +5,24 @@ function gerar_cor(opacidade = 1) {
  
     return `rgba(${r}, ${g}, ${b}, ${opacidade})`;
  }
- 
+
 function clicou() {
-    color.style.background = gerar_cor()
+    let alt = document.getElementById("colorir")
+    alt.style.backgroundColor = gerar_cor()
     
 }
 
 function segurou() {
+    let alt = document.getElementById("colorir")
+    alt.style.background = gerar_cor()
+}
 
+function entrou() {
+    let alt = document.getElementById("colorir")
+    alt.style.background = gerar_cor()
+}
+
+function saiu() {
+    let alt = document.getElementById("colorir")
     alt.style.background = gerar_cor()
 }
