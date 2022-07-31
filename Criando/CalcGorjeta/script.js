6e99abe6d5ba86762e21b86f0fb9f8bfe76d7d9e
Henrique Moraes <ahmdesouza@gmail.com>
2022-07-31 16:59:52 -0300

    update

---
+++ Criando/CalcGorjeta/script.js
@@ -0,0 +1,19 @@
+function agradecimento() {
+    var agora = new Date()
+    var h = agora.getHours()
+    var msg = window.document.getElementById('msg')
+
+
+if (h >= 6 && h < 12 ) {
+        //bom dia
+        msg.innerHTML = `Obrigado e tenha um bom dia!`
+
+    } else if (h >= 12 && h < 18 ) {
+        //boa tarde
+        msg.innerHTML = `Obrigado e tenha uma boa tarde!`
+        
+    } else {
+        //boa noite
+        msg.innerHTML = `Obrigado e tenha uma boa noite`
+    }
+}
