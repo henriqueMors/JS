ab4b1f030f7d1f3696d434ed26b8957abf7368a9
Henrique Moraes <ahmdesouza@gmail.com>
2022-07-31 18:47:42 -0300

    update

--- Criando/CalcGorjeta/script.js
+++ Criando/CalcGorjeta/script.js
@@ -19,20 +19,46 @@ if (h >= 6 && h < 12 ) {
 }
 
 function calcular() {
+    var tn1 = window.document.getElementById('txtn1')
+    var n1 = Number(tn1.value)
 
-    var vc = window.document.getElementById('vconta')
-    var porc = window.document.getElementById('nivel')
+    var tn2 = window.document.getElementById('txtn2')
+    var n2 
+}
+
+
+
+
+/*{
+
+
+    var tn2 = window.document.getElementById('txtn2')
     var resg = window.document.getElementById('resg')
     var rest = window.document.getElementById('rest')
 
-    var vc = Number(vc.value)
-    var porc = Number(porc.value)
-    var resg = Number(resg.value)
-    var rest = Number(rest.value)
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+    var rg = Number(resg.value)
+    var rt = Number(rest.value)
+    rt = n1 + rg
 
-        if (porc == 'Ótimo') {
-            resg = vc * 15
-            resg.innerHTML =`O valor da sua gorjeta ficou em R$${resg}`
+        if (n2 == 'Otimo') {
+                rg = (n1 * 100) / 15
+            
+            } else if (n2 == 'Bom') {
+                rg = (n1 * 100) / 10
+                
+            } else if (n2 == 'Regular') {
+                rg = (n1 * 100) / 5
+                
+            } else if (n2 == 'Ruim') {
+                rg = (n1 * 100) / 2
+                
+            } else {
+                rg = n1 + 0
+                
             }
+        rg.innerHTML =`O valor da sua gorjeta ficou em R$${rg}`
+        rt.innerHTML = `O valor da conta ficou em R$${rt}`
 
-}
+}*/
